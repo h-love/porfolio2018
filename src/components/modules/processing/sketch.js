bb247db2971b8efd96582fa3ba3b35022093b439
h-love <contact@henri.love>
2018-10-02 06:18:28 +0200

    Update: processing experience

--- src/components/modules/processing/sketch.js
+++ src/components/modules/processing/sketch.js
@@ -1,28 +1,87 @@
 export default (sketch) => {
   let width = 0;
   let height = 0;
-  const str = '01101100011011110111011001100101';
-  let strWidth;
-  let x = 0;
-  let y = 10;
+  // let saveX = 0;
+  // let saveY = 0;
+  let dots;
+
+  class Dot {
+    constructor(_width, _height, _x, _y, _orientation, _color) {
+      this.width = _width;
+      this.height = _height;
+      this.x = _x;
+      this.y = _y;
+      this.orientation = _orientation;
+      this.color = _color;
+    }
+
+    update = () => {
+      // if (saveX > sketch.mouseX || saveY > sketch.mouseY) {
+      //   this.orientation -= 1;
+      // } else {
+      //   this.orientation += 1;
+      // }
+
+      if (this.orientation >= 360) {
+        this.orientation = 0;
+      } else {
+        this.orientation += 1;
+      }
+    }
+
+    display = () => {
+      sketch.rectMode(sketch.CENTER);
+      sketch.push();
+      sketch.translate(this.width, this.height);
+      sketch.rotate(sketch.radians(this.orientation));
+      sketch.noStroke();
+      sketch.fill(this.color);
+      sketch.rect(0, 0, this.x, this.y);
+      sketch.pop();
+    }
+  }
+
+  sketch.preload = () => { // eslint-disable-line no-param-reassign
+  };
 
   sketch.setup = () => { // eslint-disable-line no-param-reassign
     width = document.getElementById('sketch').clientWidth;
     height = document.getElementById('sketch').clientHeight;
+    sketch.frameRate(10);
     sketch.createCanvas(width, height);
-    sketch.textFont('Rhodium Libre');
-    strWidth = sketch.textWidth(str);
-    for (let i = 0; i < height / 10; i += 1) {
-      for (let j = 0; j < width / strWidth; j += 1) {
-        sketch.fill(230);
-        sketch.text(str, x, y);
-        x += strWidth;
+    dots = [];
+    let x = 0;
+    let y = 0;
+    let item = 0;
+    for (let i = 0; i < 8; i += 1) {
+      for (let j = 0; j < 8; j += 1) {
+        dots[item] = new Dot(x,
+          y,
+          width / 2,
+          height / 2,
+          sketch.random(0, 360),
+          item % 2 === 0 ? sketch.color(255, 0, 0, 5) : sketch.color(0, 0, 255, 5));
+        x += (width / 8) + ((width / 8) / 8);
+        item += 1;
       }
       x = 0;
-      y += 10;
+      y += (height / 8) + ((height / 8) / 8);
     }
   };
 
   sketch.draw = () => { // eslint-disable-line no-param-reassign
+    sketch.background(255);
+    for (let i = 0; i < dots.length; i += 1) {
+      dots[i].update();
+      dots[i].display();
+    }
   };
+
+  //  sketch.mouseMoved = () => { // eslint-disable-line no-param-reassign
+  //    for (let i = 0; i < dots.length; i += 1) {
+  //      dots[i].update();
+  //    }
+  //    saveX = sketch.mouseX;
+  //    saveY = sketch.mouseY;
+  //  };
 };
